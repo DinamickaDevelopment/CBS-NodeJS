2ef358369fd607a6fe788e86a496574081d39f53
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-11-25 09:27:16 -0800

    update

--- 003_Console_program/003_Console_program/004_buffers_1/004_buffers_1.js
+++ 003_Console_program/003_Console_program/004_buffers_1/004_buffers_1.js
@@ -4,12 +4,13 @@
 // Метод Buffer.alloc - создает неинициализированный буфер на указанное количество байт
 const buf = Buffer.alloc(10);
 console.log('empty buffer: ' + buf); 
-console.log(buf.toString('hex')); // вывод буфера в консоль в шестнадцатеричном формате 
+
 console.log(buf);  
 
 // Метод Buffer.from(array) - создает буфер на основе массива байтов. 
-const buf1 = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]); // создает новый буфер, содержащий ASCII байты
-console.log('buffer converted to string: ' + buf1.toString()) // buffer  //hex
+const buf1 = Buffer.from([0x62, 0x75, 0x66, 0x66, 0x65, 0x72]); // создает новый буфер, содержащий ASCII байты, в которых закодировано слово buffer
+console.log('buffer converted to string: ' + buf1.toString()) // buffer  
+console.log(buf1.toString('hex')); // вывод буфера в консоль в шестнадцатеричном формате 
 
 // Метод Buffer.from(arrayBuffer[, byteOffset[, length]]) - создает буфер на основе буфера типированного массива JS 
 // При использовании такого метода буфер и типированный массив ссылаются на один и тот же сегмент памяти 

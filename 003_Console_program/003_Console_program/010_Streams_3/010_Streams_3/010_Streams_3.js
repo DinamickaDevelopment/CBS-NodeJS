5cec9a3c703eadab2e3169d343aafd6cbf24ebea
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-11-28 07:48:44 -0800

    update

---
+++ 003_Console_program/003_Console_program/010_Streams_3/010_Streams_3/010_Streams_3.js
@@ -0,0 +1,32 @@
+﻿var fs = require('fs');
+var writeData = 'This is some sample data'; 
+
+var writerStream = fs.createWriteStream('output1.txt'); 
+
+
+writerStream.write('Sample data 1 '); 
+
+writerStream.on('finish', function () {
+    console.log('write stream finish');
+}); 
+
+writerStream.on('close', function () {
+    console.log('write stream close')
+});
+
+
+// writerStream.close(); // после вызова метода close данные не будут записаны, но ошибки при попытке записи данных не произойдет  
+
+
+writerStream.write('Sample data 2 ');   
+
+// writerStream.end(); // после вызова метода end при попытке записи данных произойдет ошибка 
+
+writerStream.write('Sample data 3 ');   
+
+fs.open('output2.txt', 'w+', function (err, fd) {
+    fs.write(fd , writeData, function (err) {
+        if (err) console.log(err); 
+        console.log('write end')
+    }) 
+})

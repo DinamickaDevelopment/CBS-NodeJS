efb3aa7c1691488680c754761c452119b5b55e00
SvitlanaTimofeyeva <s.timofeyeva@dinamicka.com>
2016-11-29 08:23:25 -0800

    update

---
+++ 010_Socket.io/010_Socket.io/009_cross_domain/009_cross_domain/server1/server.js
@@ -0,0 +1,20 @@
+﻿// подключение express и socket.io 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server); 
+
+var path = require('path');
+
+var port = process.env.port || 1337; 
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', function (socket) {
+    socket.emit('greet');
+}); 
+
+server.listen(port, function () {
+    console.log('app running on port ' + port);
+}); 
---
+++ 010_Socket.io/010_Socket.io/009_cross_domain/009_cross_domain/server2/server.js
@@ -0,0 +1,16 @@
+﻿// подключение express и socket.io 
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var path = require('path');
+
+var port = process.env.port || 3000;
+
+io.on('connection', function (socket) {
+    socket.emit('greet');
+}); 
+
+server.listen(port, function () {
+    console.log('app running on port ' + port);
+})
